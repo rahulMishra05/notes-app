9fee93b38183ea7e98606aedbc94c3fa79ffefb1
Rahul Mishra <rahulmishra102000@gmail.com>
2020-11-19 17:25:47 +0530

    Adding notes part completed

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const notesEl = document.querySelector('.notes');
-const editBtn = document.querySelector('.edit');
-const deleteBtn = document.querySelector('.delete');
-const main = notesEL.querySelector(".main");
+const notesEl = document.querySelector(".notes");
+const editBtn = document.querySelector(".edit");
+const deleteBtn = document.querySelector(".delete");
+
+const main = notesEl.querySelector(".main");
 const textArea = notesEl.querySelector("textarea")
 
 // Adding functionality to edit button.
 editBtn.addEventListener('click', () => {
     main.classList.toggle("hidden");
     textArea.classList.toggle("hidden");
+});
+
+/* 
+This will convert the written text into markdown and display
+it to another place, where you can not edit the text
+*/
+textArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+
+    main.innerHTML = marked(value);
 })
